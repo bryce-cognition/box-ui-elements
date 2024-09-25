915ded7aea069b14334427efdb03a9647b9298de
Ubuntu <ubuntu@ip-10-240-154-56.us-west-2.compute.internal>
2024-09-25 13:41:03 +0000

    chore(blueprint-migration): Migrate checkboxCellRenderer.js to blueprint

--- src/elements/content-picker/checkboxCellRenderer.js
+++ src/elements/content-picker/checkboxCellRenderer.js
@@ -5,29 +5,22 @@
  */
 
 import * as React from 'react';
-import Checkbox from '../../components/checkbox/Checkbox';
+import { Checkbox } from '@box/blueprint-web/lib-esm/checkbox';
 import isRowSelectable from './cellRendererHelper';
 import type { BoxItem } from '../../common/types/core';
 
 export default (
-    onItemSelect: Function,
-    selectableType: string,
-    extensionsWhitelist: string[],
-    hasHitSelectionLimit: boolean,
-): Function => ({ rowData }: { rowData: BoxItem }) => {
-    const { name, selected = false } = rowData;
+        onItemSelect: Function,
+        selectableType: string,
+        extensionsWhitelist: string[],
+        hasHitSelectionLimit: boolean,
+    ): Function =>
+    ({ rowData }: { rowData: BoxItem }) => {
+        const { name, selected = false } = rowData;
 
-    if (!isRowSelectable(selectableType, extensionsWhitelist, hasHitSelectionLimit, rowData)) {
-        return <span />;
-    }
+        if (!isRowSelectable(selectableType, extensionsWhitelist, hasHitSelectionLimit, rowData)) {
+            return <span />;
+        }
 
-    return (
-        <Checkbox
-            hideLabel
-            isChecked={selected}
-            label={((name: any): string)}
-            name={((name: any): string)}
-            onChange={() => onItemSelect(rowData)}
-        />
-    );
-};
+        return <Checkbox checked={selected} label={name} onChange={() => onItemSelect(rowData)} />;
+    };
